7e94d7a6ff1d193243140442df2a93daaa63ba82
ykinos <yoshio@yz.velvet.jp>
2012-12-13 14:32:28 +0900

    Signed-off-by: ykinos <yoshio@yz.velvet.jp>

--- ch5/src/main/java/com/apress/prospring3/ch5/lifecycle/SimpleBeanWithJSR250.java
+++ ch5/src/main/java/com/apress/prospring3/ch5/lifecycle/SimpleBeanWithJSR250.java
@@ -15,68 +15,66 @@ import org.springframework.context.support.GenericXmlApplicationContext;
  */
 public class SimpleBeanWithJSR250 implements InitializingBean {
 
-    private static final String DEFAULT_NAME = "Luke Skywalker";
+	private static final String DEFAULT_NAME = "Luke Skywalker";
 
-    private String name = null;
+	private String name = null;
 
-    private int age = Integer.MIN_VALUE;
+	private int age = Integer.MIN_VALUE;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void myInit() {
-        System.out.println("My Init");
-    }
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 	public void afterPropertiesSet() throws Exception {
 		System.out.println("afterPropertiesSet");
 	}
 
 	@PostConstruct
-    public void init() throws Exception {
-        System.out.println("Initializing bean");
+	public void init() throws Exception {
+		System.out.println("Initializing bean");
 
-        if (name == null) {
-            System.out.println("Using default name");
-            name = DEFAULT_NAME;
-        }
+		if (name == null) {
+			System.out.println("Using default name");
+			name = DEFAULT_NAME;
+		}
 
-        if (age == Integer.MIN_VALUE) {
-            throw new IllegalArgumentException(
-                    "You must set the age property of any beans of type " + SimpleBean.class);
-        }
-    }
+		if (age == Integer.MIN_VALUE) {
+			throw new IllegalArgumentException(
+					"You must set the age property of any beans of type "
+							+ SimpleBean.class);
+		}
+	}
 
-    public String toString() {
-        return "Name: " + name + "\nAge: " + age;
-    }
+	public String toString() {
+		return "Name: " + name + "\nAge: " + age;
+	}
 
-    public static void main(String[] args) {
-    	GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-    	ctx.load("classpath:lifecycle/initJSR250.xml");
-    	ctx.refresh();
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:lifecycle/initJSR250.xml");
+		ctx.refresh();
 
-        SimpleBeanWithJSR250 simpleBean1 = getBean("simpleBean1", ctx);
-        SimpleBeanWithJSR250 simpleBean2 = getBean("simpleBean2", ctx);
-        SimpleBeanWithJSR250 simpleBean3 = getBean("simpleBean3", ctx);
-    }
+		SimpleBeanWithJSR250 simpleBean1 = getBean("simpleBean1", ctx);
+		SimpleBeanWithJSR250 simpleBean2 = getBean("simpleBean2", ctx);
+		SimpleBeanWithJSR250 simpleBean3 = getBean("simpleBean3", ctx);
+	}
 
-    private static SimpleBeanWithJSR250 getBean(String beanName,
-            ApplicationContext ctx) {
-        try {
-            SimpleBeanWithJSR250 bean = (SimpleBeanWithJSR250) ctx.getBean(beanName);
-            System.out.println(bean);
-            return bean;
-        } catch (BeanCreationException ex) {
-            System.out.println("An error occured in bean configuration: "
-                    + ex.getMessage());
-            return null;
-        }
-    }
+	private static SimpleBeanWithJSR250 getBean(String beanName,
+			ApplicationContext ctx) {
+		try {
+			SimpleBeanWithJSR250 bean = (SimpleBeanWithJSR250) ctx
+					.getBean(beanName);
+			System.out.println(bean);
+			return bean;
+		} catch (BeanCreationException ex) {
+			System.out.println("An error occured in bean configuration: "
+					+ ex.getMessage());
+			return null;
+		}
+	}
 
 }
